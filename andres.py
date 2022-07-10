3b7a14a33d1ecedcef8515ed0c575b85017202a1
AIToala <aitoala.espol.edu.ec>
2022-07-10 18:42:14 -0500

    DIJSKTRA ALTERNATIVE TERMINADO

--- andres.py
+++ andres.py
@@ -1,11 +1,11 @@
 import sys
-
+import time
 class Vertex:
     def __init__(self, node):
         self.id = node
         self.adjacent = {}
         # Set distance to infinity for all nodes
-        self.distance = sys.maxsize
+        self.distance = sys.maxsize - 10000
         # Mark all nodes unvisited        
         self.visited = False  
         # Predecessor
@@ -23,6 +23,9 @@ class Vertex:
     def get_weight(self, neighbor):
         return self.adjacent[neighbor]
 
+    def set_weight_infinite(self, neighbor, value):
+        self.adjacent[neighbor] = value
+
     def set_distance(self, dist):
         self.distance = dist
 
@@ -36,7 +39,7 @@ class Vertex:
         self.visited = True
 
     def __str__(self):
-        return str(self.id) + ' adjacent: ' + str([x.id for x in self.adjacent])
+        return str(self.id)# + ' adjacent: ' + str([x.id for x in self.adjacent])
 
     def __lt__(self, other):
         return self.distance < other.distance
@@ -76,20 +79,74 @@ class Graph:
     def set_previous(self, current):
         self.previous = current
 
-    def get_previous(self, current):
+    def get_previous(self):
         return self.previous
+    
+    def reset(self):
+        for vertex in self.vert_dict:
+            # Mark all nodes unvisited        
+            self.vert_dict[vertex].visited = False  
+            # Predecessor
+            self.vert_dict[vertex].previous = None
+            # Set infinite distance
+            self.vert_dict[vertex].distance = sys.maxsize - 10000
+            
 
-def shortest(v, path):
+    def printGraph(self):
+        for v in self.get_vertices():
+            for w in self.vert_dict[v].get_connections():
+                print("( %s , %s, %3d)"  % (v, w, self.get_vertex(v).get_weight(w)))
+            
+
+import random as rd
+
+def randomBlockNode(aGraph):
+    adj_list = aGraph.vert_dict
+    vertexes = list(adj_list.keys())
+    randomVertex = rd.choice(vertexes)
+    randomArc = rd.choice(list(adj_list[randomVertex].get_connections()))
+    adj_list[randomVertex].set_weight_infinite(randomArc, sys.maxsize)
+    print("NODO con ARCO BLOCKED -> ", randomVertex, " X X X ", randomArc.get_id())
+    for vertex in adj_list:
+        if(vertex == randomArc.get_id()):
+            adj_list[randomArc.get_id()].set_weight_infinite(aGraph.get_vertex(randomVertex), sys.maxsize - 10000)
+
+    return randomVertex, randomArc.get_id()
+def shortest_path(graph, start, target, path):
     ''' make shortest path from v.previous'''
-    if v.previous:
-        path.append(v.previous.get_id())
-        shortest(v.previous, path)
+    dijkstra(graph, start)
+
+    if target.previous:
+        path.append(target.previous.get_id())
+        shortest_path(graph, start, target.previous, path)    
+    return path[::-1]
+
+def shortest(graph, start, target, path):
+    ''' make shortest path from v.previous'''
+    path = shortest_path(graph,start,target,path)
+    print("PATH ORIGINAL - ", path)
+    while True:
+        if path:
+            element = path.pop(0)
+            element_node = graph.get_vertex(element)
+        else:
+            break
+        print("RECORRIDO: ", element, " *** RESTANTE: ", path)
+        is_path_blocked = True if rd.randint(1,3)==2 else False
+        if is_path_blocked and path:
+            nodo_b1, nodo_b2 = randomBlockNode(graph)
+            if nodo_b1 in path and nodo_b2 in path:
+                graph.reset()
+                print("BLOQUEO DETECTADO... PLANEANDO RE-RUTA *X*")
+                path = shortest_path(graph, graph.get_vertex(path[0]), target, path)
+        if element_node == target:
+            print("TERMINADO")
+            break    
     return
 
 import heapq
 
 def dijkstra(aGraph, start):
-    print("Dijkstra's shortest path")
     # Set the distance for the start node to zero 
     start.set_distance(0)
 
@@ -109,15 +166,9 @@ def dijkstra(aGraph, start):
             if next.visited:
                 continue
             new_dist = current.get_distance() + current.get_weight(next)
-            
             if new_dist < next.get_distance():
                 next.set_distance(new_dist)
                 next.set_previous(current)
-                print("updated : current = %s next = %s new_dist = %s" \
-                        %(current.get_id(), next.get_id(), next.get_distance()))
-            else:
-                print("not updated : current = %s next = %s new_dist = %s" \
-                        %(current.get_id(), next.get_id(), next.get_distance()))
 
         # Rebuild heap
         # 1. Pop every item
@@ -154,19 +205,19 @@ if __name__ == '__main__':
     g.add_edge('e', 'b', 11)
     g.add_edge('e', 'c', 11)
     g.add_edge('f', 'c', 8)
-    g.add_edge('f', 'd', 14)
-
+    g.add_edge('f', 'd', 14)    
+    
     print("Graph data:")
-    for v in g:
-        for w in v.get_connections():
-            vid = v.get_id()
-            wid = w.get_id()
-            print("( %s , %s, %3d)"  % ( vid, wid, v.get_weight(w)))
 
-            
-    dijkstra(g, g.get_vertex('b')) 
-
+    
+    
+    start_time=time.perf_counter()  
+    start = g.get_vertex('b')      
+    dijkstra(g, start) 
+    end_time=time.perf_counter()  
     target = g.get_vertex('f')
+    print("SHORTEST PATH FROM B TO F")
     path = [target.get_id()]
-    shortest(target, path)
-    print("The shortest path : %s" %(path[::-1]))
+    shortest(g, start, target, path)
+    #print("The shortest path : %s" %(path[::-1]))
+    print("CALCULATED TIME (seconds): ", end_time-start_time)
